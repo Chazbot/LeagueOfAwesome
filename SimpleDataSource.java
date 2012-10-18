1735c30e0ba225ff0c674efb04c132d8b2c6e417
Bret A. Van Hof <bretvh@hotmail.com>
2012-10-18 09:29:04 -0700

    Without private info
    Dont show private info...

--- SimpleDataSource.java
+++ SimpleDataSource.java
@@ -16,9 +16,9 @@ import java.sql.SQLException;
 public class SimpleDataSource 
 {
     private static String url = 
-            "jdbc:sqlserver://24.22.248.54 ";
-    private static String username = "sa";
-    private static String password = "44James007&&";
+            "";
+    private static String username = "";
+    private static String password = "";
     
     /**
       Gets a connection to the database.
